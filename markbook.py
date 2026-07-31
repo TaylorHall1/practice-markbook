110aa0f703757110336d6b72f79fe115717b6a19
TaylorHall1 <st23067@howick.school.nz>
2026-07-31 12:29:44 +1200

    A commit just incase I don't finish in time
    Some stuff are here, I forgot what but they are just here

--- markbook.py
+++ markbook.py
@@ -1,24 +1,58 @@
-def get_input():
+def get_testinfo():
     test_name = input("Enter the name of the test: ")
     teacher_name = input("Enter the name of the teacher: ")
-    student_name = input("Enter the name of the student: ")
+    return test_name, teacher_name
 
-    score = int(input("Enter the score of this student's test: "))
-    grade = input("Enter the grade of this student's test: ")
 
-    return test_name, teacher_name, student_name, score, grade
+def get_student_name():
+    return input("Enter the name of the student: ")
+
+
+def find_score():
+    while True:
+        try:
+            score = int(input("Enter the student's score (0-100): "))
+            if 0 <= score <= 100:
+                return score
+            print("Score has to be between 0 and 100.")
+        except ValueError:
+            print("Please enter a valid number.")
+
+
+def get_grade(score):
+    if score >= 90:
+        return "E"
+    elif score >= 70:
+        return "M"
+    elif score >= 50:
+        return "A"
+    else:
+        return "NA"
 
 
 def display_results(test_name, teacher_name, student_name, score, grade):
-    print("Test Name: ", test_name)
-    print("Teacher Name: ", teacher_name)
-    print("Student Name: ", student_name)
-    print("Score: ", score)
-    print("Grade: ", grade)
+    print("\n--- Student Result ---")
+    print("Test Name:", test_name)
+    print("Teacher Name:", teacher_name)
+    print("Student Name:", student_name)
+    print("Score:", score)
+    print("Grade:", grade)
 
 
-
-
-
-# main programs start here #
 def main():
+    test_name, teacher_name = get_testinfo()
+
+    while True:
+        student_name = get_student_name()
+        score = find_score()
+        grade = get_grade(score)
+
+        display_results(test_name, teacher_name, student_name, score, grade)
+
+        another = input("\nDo you want to add another student? (yes/no): ").lower()
+        if another != "yes":
+            print("\nProgram finished.")
+            break
+
+
+main()
